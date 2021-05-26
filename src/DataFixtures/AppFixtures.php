46d6cf79ee14a5afb86359ce73b1cf6d1865f602
TamaraChan <tamara99111@gmail.com>
2021-05-27 00:39:39 +0300

    default data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Service;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,38 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach ($this->getDataFixture() as $row) {
+            $service = (new Service())->setName($row['name']);
+            $manager->persist($service);
+        }
 
         $manager->flush();
+    }
+
+    protected function getDataFixture()
+    {
+        return [
+            [
+                'name' => 'ШАГОВАЯ ПРОГУЛКА'
+            ],
+            [
+                'name' => 'АКТИВНАЯ ПРОГУЛКА'
+            ],
+            [
+                'name' => 'ТРЕНИРОВКА'
+            ],
+            [
+                'name' => 'АБОНЕМЕНТЫ'
+            ],
+            [
+                'name' => 'ПОСТОЙ ЛОШАДИ'
+            ],
+            [
+                'name' => 'УЧАСТИЕ ЛОШАДИ В ФОТОСЕССИЯХ'
+            ],
+            [
+                'name' => 'КУПАНИЕ С ЛОШАДЬЮ'
+            ]
+        ];
     }
 }
